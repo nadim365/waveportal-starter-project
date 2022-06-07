732d17f914a9ee534b6ab06e624da829751fe831
Nadim <samnad14@gmail.com>
2022-06-07 17:43:16 -0400

    added code to connect wallet to App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,90 @@
-import * as React from "react";
-import { ethers } from "ethers";
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-export default function App() {
+const App = () => {
+  /*
+  * Just a state variable we use to store our user's public wallet.
+  */
+  const [currentAccount, setCurrentAccount] = useState("");
 
-  const wave = () => {
-    
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have MetaMask!");
+        return;
+      } else {
+        console.log("We have the Ethereum Object", ethereum);
+      }
+      /*
+      * Check if we're authorized to access the user's wallet
+      */
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an Authorized account:", account);
+        setCurrentAccount(account)
+      }
+      else {
+        console.log("No Authorized account found.")
+      }
+    }
+    catch (error) {
+      console.log(error);
+    }
   }
-  
+
+  const connectWallet = async () => {
+
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+  /*
+  * This runs our function when the page loads.
+  */
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
   return (
     <div className="mainContainer">
-
       <div className="dataContainer">
         <div className="header">
-        👋 Hey there!
+          👋 Welcome to the metaverse!
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+          I am Nadimul and this is my first Web3.0 project where I learn to create smart contracts. Connect your Ethereum wallet and wave at me!
         </div>
 
-        <button className="waveButton" onClick={wave}>
+        <button className="waveButton" onClick={null}>
           Wave at Me
         </button>
+
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
       </div>
     </div>
   );
 }
+
+export default App
